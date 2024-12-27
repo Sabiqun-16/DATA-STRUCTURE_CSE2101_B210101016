4dd94d5b0bc91a44978db388a1f95f3c126e0b6a
Sabiqun-16 <sabiqunnahar65@gmail.com>
2024-12-27 20:15:59 +0600

    Add files via upload

--- Linked_List/Singly-Linked_list/Delete_Last_Node_L_L.cpp
+++ Linked_List/Singly-Linked_list/Delete_Last_Node_L_L.cpp
@@ -1,1 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* next;
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+void Print(Node* head){
+    struct Node* ptr=head;
+    while(ptr!=NULL){
+        cout<<ptr->data<<" ";
+        ptr= ptr->next;
+    }
+    cout<<"NULL"<<endl;
+}
+Node* DeleteLastNode(Node* head){
+    if(head==NULL){
+        cout<<"Memory Allocation Failed"<<endl;
+        return NULL;
+    }
+    Node* ptr=head;
+    while(ptr->next!=NULL && ptr->next->next!=NULL){
+        ptr=ptr->next;
+    }
+    delete ptr->next;
+    ptr->next=NULL;
+    return head;
+}
+int main()
+{
+    cout<<"Link List ";
+    Node* head= new Node(4);
+    head->next= new Node(7);
+    head->next->next= new Node(8);
+    head->next->next->next= new Node(2);
+    Print(head);
+
+    cout<<"After Delete :";
+    head=DeleteLastNode(head);
+    Print(head);
+    return 0;
+}
 
