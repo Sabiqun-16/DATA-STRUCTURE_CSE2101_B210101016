5676017791f7ba6cc96ac74e97f0d73b0dcaa5ed
Sabiqunnahar <sabiqunnahar65@gmail.com>
2025-02-21 19:29:00 +0600

    Update Pop_Stack.cpp

--- Stack/Pop_Stack.cpp
+++ Stack/Pop_Stack.cpp
@@ -11,14 +11,6 @@ void Push(int v){
     }
 }
 
-void Pop(){
-    if(top<=-1) cout<<"Underflow"<<endl;
-    else{
-        cout<<stack[top]<<endl;
-        top--;
-    }
-}
-
 void Print(){
     if (top>=0){
         cout<<"Stack elements are: ";
@@ -28,40 +20,32 @@ void Print(){
     else cout<<"Stack is empty"<<endl;
 }
 
+void Pop(){
+    if(top<=-1) cout<<"Underflow"<<endl;
+    else{
+        cout<<stack[top]<<endl;
+        top--;
+    }
+}
+
 int main()
 {
-    int arr[]={1,2,3,4,5,6};
-    int s=sizeof(arr)/sizeof(arr[0]);
-    for (int i = 0; i < s; i++) {
-        if(top>=n-1)
-            cout<<"Overflow"<<endl;
-        else {
-            top++;
-            stack[top]=arr[i];
-        }
-    }
-
-    cout<<"1) Display stack"<<endl;
-    cout<<"2) Push in stack"<<endl;
-    cout<<"3) Pop in stack"<<endl;
+    cout<<"1) Push in stack"<<endl;
+    cout<<"2) Pop in stack"<<endl;
+    cout<<"3) Display stack"<<endl;
     cout<<"4) Exit"<<endl;
-    int ch;
+    int k,ch;
     while(true){
         cout <<"\nEnter choice: ";
         cin>>ch;
 
-        if(ch==1) Print();
-        if(ch==2){
+        if(ch==1){
             cout<<"Enter value to be pushed: ";
             cin>>k;
             Push(k);
         }
-
-         else if(ch==3){
-            cout<<"Enter value to be poped: ";
-            Pop();
-        }
-             
+        else if(ch==2) Pop();
+        else if(ch==3) Print();
         else if (ch==4){
             cout<<"Exiting the program..."<<endl;
             break;
@@ -69,5 +53,3 @@ int main()
         else cout<<"Invalid Choice"<<endl;
     }
 }
-
-
