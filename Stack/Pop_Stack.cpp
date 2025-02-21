5e91282eea62eb14a0911311d40f1724adae7606
Sabiqunnahar <sabiqunnahar65@gmail.com>
2025-02-21 19:25:29 +0600

    Update Pop_Stack.cpp

--- Stack/Pop_Stack.cpp
+++ Stack/Pop_Stack.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int stack[100],n=100,top=-1;
 
+void Push(int v){
+    if(top>=n-1) cout<<"Overflow"<<endl;
+    else{
+        top++;
+        stack[top]=v;
+    }
+}
+
 void Pop(){
     if(top<=-1) cout<<"Underflow"<<endl;
     else{
@@ -14,7 +22,7 @@ void Pop(){
 void Print(){
     if (top>=0){
         cout<<"Stack elements are: ";
-        for(int i=top;i>=0;i--) cout<<stack[i]<<" ";
+        for(int i=0;i<=top;i++) cout<<stack[i]<<" ";
         cout<<endl;
     }
     else cout<<"Stack is empty"<<endl;
@@ -34,19 +42,27 @@ int main()
     }
 
     cout<<"1) Display stack"<<endl;
-    cout<<"2) Pop in stack"<<endl;
-    cout<<"3) Exit"<<endl;
+    cout<<"2) Push in stack"<<endl;
+    cout<<"3) Pop in stack"<<endl;
+    cout<<"4) Exit"<<endl;
     int ch;
     while(true){
         cout <<"\nEnter choice: ";
         cin>>ch;
 
         if(ch==1) Print();
-        else if(ch==2){
+        if(ch==2){
+            cout<<"Enter value to be pushed: ";
+            cin>>k;
+            Push(k);
+        }
+
+         else if(ch==3){
             cout<<"Enter value to be poped: ";
             Pop();
         }
-        else if (ch==3){
+             
+        else if (ch==4){
             cout<<"Exiting the program..."<<endl;
             break;
         }
